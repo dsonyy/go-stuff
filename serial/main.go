9ed5e76dc2e6d550a7e4e99271da4fb4686a26ff
Szymon Bednorz <dsonyyy@gmail.com>
2021-05-24 15:00:37 +0200

    Playing with serial

---
+++ serial/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jacobsa/go-serial/serial"
+)
+
+func main() {
+	// Setting up the port
+	options := serial.OpenOptions{
+		PortName:        "COM1",
+		BaudRate:        19200,
+		DataBits:        8,
+		MinimumReadSize: 4,
+	}
+	port, err := serial.Open(options)
+	defer port.Close()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Loop
+	for {
+		var line string
+		_, err := fmt.Scanln(&line)
+		if err != nil {
+			log.Println(err)
+			break
+		}
+
+		n, err := port.Write([]byte(line))
+		if err != nil {
+			log.Println(err)
+			break
+		}
+		log.Println("Wrote", n, "bytes:", []byte(line))
+
+		buf := make([]byte, 128)
+		n, err = port.Read(buf)
+		if err != nil {
+			log.Println(err)
+			break
+		}
+		log.Println("Recieved:", buf)
+	}
+}
