72ca8a9bea9c781642daf344dc7e7fe2ce7d5cb5
Szymon Bednorz <dsonyyy@gmail.com>
2021-05-27 00:54:04 +0200

    Calculating accelerometer values.

--- serial-read-accelerometer/go.mod
+++ serial-read-accelerometer/go.mod
@@ -4,6 +4,5 @@ go 1.15
 
 require (
 	github.com/jacobsa/go-serial v0.0.0-20180131005756-15cf729a72d4
-	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07 // indirect
 	golang.org/x/sys v0.0.0-20210521203332-0cec03c779c1 // indirect
 )
--- serial-read-accelerometer/main.go
+++ serial-read-accelerometer/main.go
@@ -31,19 +31,33 @@ func main() {
 		}
 
 		if buf[0] == 'L' && buf[1] == 'D' && buf[2] == '-' {
-			xGyro := mergeBytes(buf[3], buf[4])
-			yGyro := mergeBytes(buf[5], buf[6])
-			zGyro := mergeBytes(buf[7], buf[8])
-			xAcc := mergeBytes(buf[9], buf[10])
-			yAcc := mergeBytes(buf[11], buf[12])
-			zAcc := mergeBytes(buf[13], buf[14])
+			xAccRaw := mergeBytes(buf[3], buf[4])
+			yAccRaw := mergeBytes(buf[5], buf[6])
+			zAccRaw := mergeBytes(buf[7], buf[8])
+			xGyroRaw := mergeBytes(buf[9], buf[10])
+			yGyroRaw := mergeBytes(buf[11], buf[12])
+			zGyroRaw := mergeBytes(buf[13], buf[14])
 
-			log.Printf("xg: %5d, yg: %5d: zg: %5d, xa: %5d, ya: %5d, za: %5d\n", xGyro, yGyro, zGyro, xAcc, yAcc, zAcc)
+			// xAcc := float64(xAccRaw-0) / 16384.0
+			// yAcc := float64(yAccRaw-0) / 16384.0
+			// zAcc := float64(zAccRaw-0) / 16384.0
+			// xGyro := float64(xGyroRaw+42) / 16.4
+			// yGyro := float64(yGyroRaw-9) / 16.4
+			// zGyro := float64(zGyroRaw+29) / 16.4
+
+			xAcc := float64(xAccRaw) * 9.8 * 2 / 32768
+			yAcc := float64(yAccRaw) * 9.8 * 2 / 32768
+			zAcc := float64(zAccRaw) * 9.8 * 2 / 32768
+			xGyro := float64(xGyroRaw) * (3.14159 / 180) / 1000 / 32768
+			yGyro := float64(yGyroRaw) * (3.14159 / 180) / 1000 / 32768
+			zGyro := float64(zGyroRaw) * (3.14159 / 180) / 1000 / 32768
+
+			log.Printf("LD-   %5.1f %5.1f %5.1f   %5.1f %5.1f %5.1f   S\n", xGyro, yGyro, zGyro, xAcc, yAcc, zAcc)
 		}
 	}
 }
 
-/// mergeBytes merges two uint8s to one uint16.
+// mergeBytes merges two uint8s to one uint16.
 func mergeBytes(left8 byte, right8 byte) uint16 {
-	return uint16(left8) + uint16(right8)<<8
+	return (uint16(left8) << 8) | uint16(right8)
 }
